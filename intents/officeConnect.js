f95c8c82dbf6f3901515500bc15133c7a7602c5b
Agneysh <Agneysh96@gmail.com>
2019-06-19 09:57:46 +0400

    ref: clean up

--- intents/officeConnect.js
+++ intents/officeConnect.js
@@ -1,5 +1,6 @@
 const generic = require('../helpers/generic');
 
+
 const OfficeConnectIntent = {
     canHandle(handlerInput) {
         const {
@@ -15,56 +16,38 @@ const OfficeConnectIntent = {
 
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-        const generic = require('../helpers/generic');
+        // if there is session and value is present in input, overwrite the session one to the new value.
+        if (handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].status.code == "ER_SUCCESS_MATCH" && sessionAttributes.officeName) {
+            // incase the values are the same. return the message
+            if (sessionAttributes.officeName == handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].values[0].value.name) {
+                contact = await generic.getProximityOfficeContact(sessionAttributes.officeName);
+                message = "You can contact " + sessionAttributes.officeName + " on " + contact;
+            }
+            //set the new session value to the input value
+            else {
+                officeSlot = handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].values[0].value.name;
 
-        const OfficeConnectIntent = {
-            canHandle(handlerInput) {
-                const {
-                    request
-                } = handlerInput.requestEnvelope;
+                sessionAttributes.officeName = officeSlot;
+                handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
 
-                return request.type === 'IntentRequest' && (request.intent.name === 'OfficeConnectIntent');
-            },
-            async handle(handlerInput) {
-                var officeSlot = "";
-                var message = "";
-                var contact = "";
-
-                const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-                // if there is session and value is present in input, overwrite the session one to the new value.
-                if (handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].status.code == "ER_SUCCESS_MATCH" && sessionAttributes.officeName) {
-                    // incase the values are the same. return the message
-                    if (sessionAttributes.officeName == handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].values[0].value.name) {
-                        contact = await generic.getProximityOfficeContact(sessionAttributes.officeName);
-                        message = "You can contact " + sessionAttributes.officeName + " on " + contact;
-                    }
-                    //set the new session value to the input value
-                    else {
-                        officeSlot = handlerInput.requestEnvelope.request.intent.slots.office.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-
-                        sessionAttributes.officeName = officeSlot;
-                        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-
-                        contact = await generic.getProximityOfficeContact(officeSlot);
-                        message = "You can contact " + officeSlot + " on " + contact;
-                    }
-                }
-                //If present in session, get the value.
-                else if (sessionAttributes.officeName) {
-                    contact = await generic.getProximityOfficeContact(sessionAttributes.officeName);
-                    message = "You can contact " + sessionAttributes.officeName + " on " + contact;
-                }
-
-                //If non-existant. return Does not exist.
-                else {
-                    officeSlot = handlerInput.requestEnvelope.request.intent.slots.office.value;
-                    message = "I'm Sorry but " + officeSlot + " does not exist.";
-                }
-                return handlerInput.responseBuilder.speak(message).getResponse();
-            },
+                contact = await generic.getProximityOfficeContact(officeSlot);
+                message = "You can contact " + officeSlot + " on " + contact;
+            }
         }
-    }
+        //If present in session, get the value.
+        else if (sessionAttributes.officeName) {
+            contact = await generic.getProximityOfficeContact(sessionAttributes.officeName);
+            message = "You can contact " + sessionAttributes.officeName + " on " + contact;
+        }
+
+        //If non-existant. return Does not exist.
+        else {
+            officeSlot = handlerInput.requestEnvelope.request.intent.slots.office.value;
+            message = "I'm Sorry but " + officeSlot + " does not exist.";
+        }
+        return handlerInput.responseBuilder.speak(message).getResponse();
+    },
+
 };
 
 module.exports = OfficeConnectIntent;
